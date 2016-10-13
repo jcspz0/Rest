8332574f49ebe408c631bc60b975238d16832e06
Juan Carlos Suarez <jc.suarez.developer@gmail.com>
2016-10-13 08:53:42 -0400

    base de datos
    base de datos base para poder recibir los datos de las tareas

---
+++ callback.sql
@@ -0,0 +1,147 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50624
+Source Host           : localhost:3306
+Source Database       : umov
+
+Target Server Type    : MYSQL
+Target Server Version : 50624
+File Encoding         : 65001
+
+Date: 2016-10-12 10:47:49
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for callbacks
+-- ----------------------------
+DROP TABLE IF EXISTS `callbacks`;
+CREATE TABLE `callbacks` (
+`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
+`task`  varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+`activity_history`  bigint(20) NOT NULL ,
+`created_at`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+`updated_at`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_unicode_ci
+AUTO_INCREMENT=11
+
+;
+
+-- ----------------------------
+-- Records of callbacks
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for logs
+-- ----------------------------
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE `logs` (
+`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
+`log`  varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+`created_at`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+`updated_at`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_unicode_ci
+AUTO_INCREMENT=3
+
+;
+
+-- ----------------------------
+-- Records of logs
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for migrations
+-- ----------------------------
+DROP TABLE IF EXISTS `migrations`;
+CREATE TABLE `migrations` (
+`migration`  varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+`batch`  int(11) NOT NULL 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_unicode_ci
+
+;
+
+-- ----------------------------
+-- Records of migrations
+-- ----------------------------
+BEGIN;
+INSERT INTO `migrations` VALUES ('2014_10_12_000000_create_users_table', '1'), ('2014_10_12_100000_create_password_resets_table', '1'), ('2016_10_12_121117_create_callback_table', '1'), ('2016_10_12_122752_create_logs_table', '2');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for password_resets
+-- ----------------------------
+DROP TABLE IF EXISTS `password_resets`;
+CREATE TABLE `password_resets` (
+`email`  varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+`token`  varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+`created_at`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+INDEX `password_resets_email_index` (`email`) USING BTREE ,
+INDEX `password_resets_token_index` (`token`) USING BTREE 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_unicode_ci
+
+;
+
+-- ----------------------------
+-- Records of password_resets
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+`id`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
+`name`  varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+`email`  varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+`password`  varchar(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL ,
+`remember_token`  varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL ,
+`created_at`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+`updated_at`  timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+PRIMARY KEY (`id`),
+UNIQUE INDEX `users_email_unique` (`email`) USING BTREE 
+)
+ENGINE=InnoDB
+DEFAULT CHARACTER SET=utf8 COLLATE=utf8_unicode_ci
+AUTO_INCREMENT=1
+
+;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+BEGIN;
+COMMIT;
+
+-- ----------------------------
+-- Auto increment value for callbacks
+-- ----------------------------
+ALTER TABLE `callbacks` AUTO_INCREMENT=11;
+
+-- ----------------------------
+-- Auto increment value for logs
+-- ----------------------------
+ALTER TABLE `logs` AUTO_INCREMENT=3;
+
+-- ----------------------------
+-- Auto increment value for users
+-- ----------------------------
+ALTER TABLE `users` AUTO_INCREMENT=1;
